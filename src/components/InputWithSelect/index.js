30b4add4645bb5c49ac8a5d67c163e9461051b26
Adeyemi Babalola <babalolasimeon@gmail.com>
2021-05-30 23:28:58 +0100

    feat (components): add InputWithSelect

---
+++ src/components/InputWithSelect/index.js
@@ -0,0 +1,44 @@
+import styles from './InputWithSelect.module.scss';
+
+const InputWithSelect = ({ amount, label, currencyOptions, selectedCurrency, handleCurrencyChange, handleInputChange }) => {
+
+  const showCurrencyOptions = () => {
+    return currencyOptions.length > 0 ? (
+      currencyOptions.map((currency) => (
+        <option key={currency} value={currency}>
+          {currency}
+        </option>
+      ))
+    ) : (
+      <option value="">No data</option>
+    );
+  };
+
+  return (
+    <div className={styles.InputWithSelect}>
+      <label htmlFor="currencyAmount" className={styles.label}>
+        {label}
+      </label>
+      <input
+        name="currencyAmount"
+        value={amount}
+        type="number"
+        aria-label="Currency Amount Field"
+        className={styles.input}
+        onChange={handleInputChange}
+      />
+      <select
+        name="currencyType"
+        id="currencyType"
+        aria-label="Currency Type"
+        className={styles.select}
+        value={selectedCurrency}
+        onChange={handleCurrencyChange}
+      >
+        {showCurrencyOptions()}
+      </select>
+    </div>
+  );
+};
+
+export default InputWithSelect;
